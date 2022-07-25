401fd5da29072a2e1e42aefce08b22cd032edb49
Ravi Kumar <ravi.kumar2@kelltontech.com>
2022-07-25 21:48:43 +0530

    seprate modules

--- src/modules/tempTracker/temptracker.test.tsx
+++ src/modules/tempTracker/temptracker.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { TempTrackerComponent } from ".";
-describe("AnagramComponent test", () => {
+describe("TempTrackerComponent test", () => {
   describe("First test", () => {
     it("should render", () => {
       const { container } = render(<TempTrackerComponent />);
